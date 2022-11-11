b1d6a5d1a45c1d8eebb9c4d3e98a7284db36d535
narcisabadea <vasile.narcisa11@gmail.com>
2022-11-11 11:20:52 +0200

    Chunks

--- chunk/index.js
+++ chunk/index.js
@@ -1,6 +1,7 @@
 // --- Directions
 // Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
+
 // --- Examples
 // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
@@ -8,6 +9,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1
+function chunk(array, size) {
+  let newArr = [];
+
+  for (let elem of array) {
+    const last = newArr[newArr.length - 1];
+
+    if (!last || last.length === size) {
+      newArr.push([elem]);
+    } else {
+      last.push(elem);
+    }
+  }
+  return newArr;
+}
+
+// Solution 2
+function chunk2(array, size) {
+  let newArr = [];
+  let index = 0;
+
+  while (index < array.length) {
+    newArr.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return newArr;
+}
 
 module.exports = chunk;
