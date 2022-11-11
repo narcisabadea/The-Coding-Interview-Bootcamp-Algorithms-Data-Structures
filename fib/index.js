fafcce655236586cf6b058407efe509a608d167b
narcisabadea <vasile.narcisa11@gmail.com>
2022-11-11 21:36:55 +0200

    Fibonacci

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Solution 1:
+function fib(n) {
+  let arr = [0, 1];
+
+  for (let i = 1; i <= n; i++) {
+    let a = arr[arr.length - 1];
+    let b = arr[arr.length - 2];
+    arr.push(a + b);
+  }
+
+  return arr[n];
+}
+
+// Solution 2:
+function fib2(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
 
 module.exports = fib;
